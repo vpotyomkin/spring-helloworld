6188c347dda79a6952efeff7089cf922618dbfcb
Victor Potyomkin <victor.poyomkin@icloud.com>
2021-11-24 21:30:12 +0300

    Done.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catbean = (Cat) applicationContext.getBean("cat");
+        Cat catbean1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean1);
+        System.out.println(catbean == catbean1);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    private String name;
+
+    public Cat(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName() { return this.name; }
+
+    public String toString() { return this.name; }
+}
